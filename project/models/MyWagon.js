4dc73065a9f1a767a9ac6f5781c97bb5d764efc2
Vasco Sá <vasco.liberal.sa@gmail.com>
2026-04-25 10:30:22 +0100

    style: make back wheels 1.2x larger than front wheels

--- project/models/MyWagon.js
+++ project/models/MyWagon.js
@@ -26,29 +26,35 @@ export class MyWagon extends CGFobject {
         // 4 Wheels
         const xPos = 1.5;
         const zPos = 1.1;
-        const yPos = 0.5;
-
+        
+        // Front Wheels (Standard size)
+        const frontY = 0.5;
         // Front Right
         this.scene.pushMatrix();
-        this.scene.translate(xPos, yPos, zPos);
+        this.scene.translate(xPos, frontY, zPos);
         this.wheel.display();
         this.scene.popMatrix();
 
         // Front Left
         this.scene.pushMatrix();
-        this.scene.translate(xPos, yPos, -zPos);
+        this.scene.translate(xPos, frontY, -zPos);
         this.wheel.display();
         this.scene.popMatrix();
 
+        // Back Wheels (1.2x larger)
+        const backY = 0.6;
+        const backScale = 1.2;
         // Back Right
         this.scene.pushMatrix();
-        this.scene.translate(-xPos, yPos, zPos);
+        this.scene.translate(-xPos, backY, zPos);
+        this.scene.scale(backScale, backScale, backScale);
         this.wheel.display();
         this.scene.popMatrix();
 
         // Back Left
         this.scene.pushMatrix();
-        this.scene.translate(-xPos, yPos, -zPos);
+        this.scene.translate(-xPos, backY, -zPos);
+        this.scene.scale(backScale, backScale, backScale);
         this.wheel.display();
         this.scene.popMatrix();
 
